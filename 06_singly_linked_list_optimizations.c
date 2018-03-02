5f2fd565437b6e32ad1a835ceef93092b0d9fd14
Aliaksandr Klimovich <aliaksandr_klimovich@icloud.com>
2018-03-02 16:45:28 +0300

    Singly linked list. Optimization. Make file structure.

---
+++ 06_singly_linked_list_optimizations.c
@@ -0,0 +1,19 @@
+/*
+	Singly linked list
+	==================
+	
+	Optimized lined list from previous lesson.
+	
+	
+	
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	
+	
+	return 0;	
+}
+
